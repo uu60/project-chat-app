4a11f56e29f10fb8a17b0c16968a652311ee6a2d
wzy7955 <2789577758@qq.com>
2023-04-15 20:10:30 +0800

    button click

--- TeamApplication/app/src/main/java/com/ph/teamapplication/activity/LoginActivity.java
+++ TeamApplication/app/src/main/java/com/ph/teamapplication/activity/LoginActivity.java
@@ -4,36 +4,71 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.Looper;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.ph.teamapplication.R;
 import com.ph.teamapplication.utils.Requests;
 import com.ph.teamapplication.utils.Instances;
+import com.ph.teamapplication.utils.StringUtils;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
 
     private Button btnLogin;
+    private Button btnReg;
+    private EditText etUsername;
+    private EditText etPwd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
         btnLogin = findViewById(R.id.btn_login);
+        btnReg = findViewById(R.id.btn_reg);
+        etUsername = findViewById(R.id.et_username);
+        etPwd = findViewById(R.id.et_pwd);
 
-        btnLogin.setOnClickListener(view -> {
+        btnLogin.setOnClickListener(v -> {
             Instances.pool.execute(() -> {
-                try {
-                    String response = Requests.post("http://192.168.1.102:8080/hello");
-                    Looper.prepare();
-                    Toast.makeText(this, response, Toast.LENGTH_SHORT).show();
-                    Looper.loop();
-                } catch (Throwable e) {
-                    throw new RuntimeException(e);
-                }
-            });
+                String username = etUsername.getText().toString();
+                String password = etPwd.getText().toString();
 
+                if (StringUtils.isEmpty(username)) {
+
+                    
+                    return;
+                }
+
+                if (username.length() < 5) {
+
+
+                    return;
+                }
+
+                if (StringUtils.isEmpty(password)) {
+
+
+                    return;
+                }
+
+                if (password.length() < 8) {
+
+
+                    return;
+                }
+
+                Map<String, String> params = new HashMap<>();
+                params.put("username", username);
+                params.put("password", password);
+
+                Requests.post("", params);
+
+            });
         });
     }
 }
