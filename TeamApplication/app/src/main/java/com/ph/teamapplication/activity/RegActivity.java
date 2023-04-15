faf48c41c5af553a1cd122903cb8487e86e38b21
wzy7955 <2789577758@qq.com>
2023-04-15 21:58:38 +0800

    regActivity initial

---
+++ TeamApplication/app/src/main/java/com/ph/teamapplication/activity/RegActivity.java
@@ -0,0 +1,14 @@
+package com.ph.teamapplication.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.ph.teamapplication.R;
+
+public class RegActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reg);
+    }
+}
