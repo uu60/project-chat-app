fb9e197ed20a5d1a9059e0716380bfcbcb6a1877
uu60 <djz6660@icloud.com>
2023-04-15 15:44:02 +0800

    backend init

---
+++ spring-boot-team-app-backend/src/main/java/com/ph/springbootteamappbackend/SpringBootTeamAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ph.springbootteamappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTeamAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTeamAppBackendApplication.class, args);
+    }
+
+}
---
+++ spring-boot-team-app-backend/src/test/java/com/ph/springbootteamappbackend/SpringBootTeamAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ph.springbootteamappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTeamAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
