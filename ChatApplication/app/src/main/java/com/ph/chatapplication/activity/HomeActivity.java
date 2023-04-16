6c4e9870df550655263608842a6f3782c792adf4
uu60 <djz6660@icloud.com>
2023-04-17 00:54:33 +0800

    add home activity with navigation

--- ChatApplication/app/src/main/java/com/ph/chatapplication/activity/HomeActivity.java
+++ ChatApplication/app/src/main/java/com/ph/chatapplication/activity/HomeActivity.java
@@ -1,9 +1,14 @@
 package com.ph.chatapplication.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.ph.chatapplication.R;
 
 public class HomeActivity extends AppCompatActivity {
@@ -12,5 +17,18 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // get navigation view
+        BottomNavigationView navView = findViewById(R.id.nav_home);
+
+        // build fragment container
+        NavController navController = Navigation.findNavController(this, R.id.frag_container_home);
+
+        // start up
+        NavigationUI.setupWithNavController(navView, navController);
     }
 }
