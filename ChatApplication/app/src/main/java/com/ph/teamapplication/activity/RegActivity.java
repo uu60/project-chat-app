e5774ab89ba9f5733207bdb942116977ab50c00d
wzy7955 <2789577758@qq.com>
2023-04-16 17:26:34 +0800

    注册页面

---
+++ ChatApplication/app/src/main/java/com/ph/teamapplication/activity/RegActivity.java
@@ -0,0 +1,16 @@
+package com.ph.teamapplication.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ph.teamapplication.R;
+
+public class RegActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reg);
+    }
+}
---
+++ ChatApplication/app/src/main/java/com/ph/teamapplication/activity/chatActivity.java
@@ -0,0 +1,16 @@
+package com.ph.teamapplication.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ph.teamapplication.R;
+
+public class chatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
