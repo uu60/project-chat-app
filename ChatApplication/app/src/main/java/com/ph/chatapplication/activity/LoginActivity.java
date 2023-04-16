6568c9668b3d7f0263a8fa6b6e2e35b7f4dac4da
April-ROY <ljt702090871@163.com>
2023-04-17 01:18:55 +0800

    add token search before login

--- ChatApplication/app/src/main/java/com/ph/chatapplication/activity/LoginActivity.java
+++ ChatApplication/app/src/main/java/com/ph/chatapplication/activity/LoginActivity.java
@@ -1,14 +1,11 @@
 package com.ph.chatapplication.activity;
 
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
-import android.view.View;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -24,8 +21,10 @@ import com.ph.chatapplication.utils.StringUtils;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class LoginActivity extends AppCompatActivity {
+    private SharedPreferences preference;
 
     private Button btnLogin;
     private Button btnReg;
@@ -33,11 +32,16 @@ public class LoginActivity extends AppCompatActivity {
     private EditText etPwd;
     private Handler handler;
 
-    private SharedPreferences preference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        preference = getSharedPreferences("token", MODE_PRIVATE);
+        AtomicReference<String> token = new AtomicReference<>(reload());
+        if (token.get() != null){
+            doRegister();
+        }
         setContentView(R.layout.activity_login);
         btnLogin = findViewById(R.id.btn_login);
         btnReg = findViewById(R.id.btn_reg);
@@ -64,6 +68,7 @@ public class LoginActivity extends AppCompatActivity {
         String username = etUsername.getText().toString();
         String password = etPwd.getText().toString();
 
+
         if (StringUtils.isEmpty(username) || username.length() < 5) {
             sendToHandler("Username must longer than 4 characters.");
             return;
@@ -88,12 +93,15 @@ public class LoginActivity extends AppCompatActivity {
                 String s = "success";
                 Log.d("login", s);
                 String token = (String) resp.getData();
-
-                preference = getSharedPreferences(username, MODE_PRIVATE);
+                String name = "token";
+                preference = getSharedPreferences(name, MODE_PRIVATE);
                 SharedPreferences.Editor editor = preference.edit();
                 editor.putString("token", token);
                 editor.apply();
+
                 // TODO: 跳转到联系人列表
+//                Intent intent = new Intent(this, );
+//                startActivity(intent);
 
             } else if (resp.getCode() == ErrorCodeConst.LOGIN_FAILED) {
                 String s = "fail";
@@ -125,12 +133,16 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
-    private void reload() {
+    private String reload() {
         String token = preference.getString("token", null);
-        Log.d("token", token);
         if (token != null) {
             Log.d("token", token);
+            return token;
+        }else {
+            String s = "null";
+            Log.d("token", s);
         }
+        return token;
     }
 
     private void sendToHandler(String msg) {
