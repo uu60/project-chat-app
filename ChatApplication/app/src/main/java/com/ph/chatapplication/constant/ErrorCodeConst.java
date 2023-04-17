89c3ae11e3d37d81b319fad31c10d25009ec2a9a
uu60 <djz6660@icloud.com>
2023-04-17 18:00:17 +0800

    add error code

--- ChatApplication/app/src/main/java/com/ph/chatapplication/constant/ErrorCodeConst.java
+++ ChatApplication/app/src/main/java/com/ph/chatapplication/constant/ErrorCodeConst.java
@@ -5,9 +5,11 @@ package com.ph.chatapplication.constant;
  * @since 2023/4/15 23:09
  */
 public class ErrorCodeConst {
-
     public static final int SUCCESS = 0;
     public static final int JWT_TOKEN_INVALID = 101;
     public static final int LOGIN_FAILED = 102;
     public static final int REGISTER_FAILED = 103;
+    public static final int CONTACT_ADD_FAILED = 104;
+    public static final int PORTRAIT_CHANGE_FAILED = 105;
+    public static final int DETAILS_CHANGE_FAILED = 105;
 }
