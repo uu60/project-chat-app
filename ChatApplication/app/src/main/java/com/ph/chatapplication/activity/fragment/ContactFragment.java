38fbf60aa2ca90a90cc6c53d1e40a70c2f2e2997
uu60 <djz6660@icloud.com>
2023-04-17 12:54:39 +0800

    update navigator

--- ChatApplication/app/src/main/java/com/ph/chatapplication/activity/fragment/ContactFragment.java
+++ ChatApplication/app/src/main/java/com/ph/chatapplication/activity/fragment/ContactFragment.java
@@ -3,18 +3,27 @@ package com.ph.chatapplication.activity.fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.ph.chatapplication.R;
 
+import org.w3c.dom.Text;
+
 public class ContactFragment extends Fragment {
+
+    private TextView tvHead;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        FragmentActivity activity = getActivity();
+        tvHead = activity.findViewById(R.id.tv_head);
+        tvHead.setText("Contact");
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_contact, container, false);
     }
