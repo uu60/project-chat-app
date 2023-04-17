cbecfd82081a3c5a60be23a01ca0a5bf6f7e7ed1
uu60 <djz6660@icloud.com>
2023-04-17 20:18:48 +0800

    optimize code

--- ChatApplication/app/src/main/java/com/ph/chatapplication/activity/fragment/AddContactFragment.java
+++ ChatApplication/app/src/main/java/com/ph/chatapplication/activity/fragment/AddContactFragment.java
@@ -16,7 +16,6 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.ph.chatapplication.R;
 import com.ph.chatapplication.activity.adapter.AddContactFragmentAdapter;
-import com.ph.chatapplication.activity.adapter.ContactFragmentAdapter;
 import com.ph.chatapplication.utils.Instances;
 
 import java.util.List;
