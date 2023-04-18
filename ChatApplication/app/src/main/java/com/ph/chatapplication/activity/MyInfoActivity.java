98619a6b9ee592d6c04c67cbcca38cd8c06797dc
wzy7955 <2789577758@qq.com>
2023-04-18 11:56:17 +0800

    change icon

--- ChatApplication/app/src/main/java/com/ph/chatapplication/activity/MyInfoActivity.java
+++ ChatApplication/app/src/main/java/com/ph/chatapplication/activity/MyInfoActivity.java
@@ -1,14 +1,246 @@
 package com.ph.chatapplication.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Fragment;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.imageview.ShapeableImageView;
 import com.ph.chatapplication.R;
+import com.ph.chatapplication.utils.BitmapUtils;
+import com.ph.chatapplication.utils.CameraUtils;
+
+import com.tbruyelle.rxpermissions3.RxPermissions;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 public class MyInfoActivity extends AppCompatActivity {
+
+    private RxPermissions rxPermissions;
+
+    //是否拥有权限
+    private boolean hasPermissions = false;
+
+    //底部弹窗
+    private BottomSheetDialog bottomSheetDialog;
+    //弹窗视图
+    private View bottomView;
+    //存储拍完照后的图片
+    private File outputImagePath;
+    //启动相机标识
+    public static final int TAKE_PHOTO = 1;
+    //启动相册标识
+    public static final int SELECT_PHOTO = 2;
+
+    //图片控件
+    private ShapeableImageView ivHead;
+    //Base64
+    private String base64Pic;
+    //拍照和相册获取图片的Bitmap
+    private Bitmap orc_bitmap;
+
+    //Glide请求图片选项配置
+    private RequestOptions requestOptions = RequestOptions.circleCropTransform()
+            .diskCacheStrategy(DiskCacheStrategy.NONE)//不做磁盘缓存
+            .skipMemoryCache(true);//不做内存缓存
+    private ImageButton ib_portrait;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_info);
+        checkVersion();
+        ib_portrait = findViewById(R.id.ib_portrait);
     }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 555: {
+                if (grantResults != null && grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // 权限被用户同意，可以做你要做的事情了。
+                    hasPermissions = true;
+                    Toast.makeText(this, "成了", Toast.LENGTH_SHORT).show();
+                } else {
+                    // 权限被用户拒绝了，可以提示用户,关闭界面等等。
+                    Toast.makeText(this, "他妈的", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+        }
+    }
+
+
+    /**
+     * Toast提示
+     *
+     * @param msg
+     */
+    private void showMsg(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * 检查版本
+     */
+    private void checkVersion() {
+//            //如果你是在Fragment中，则把this换成getActivity()
+//            rxPermissions = new RxPermissions(this);
+//            //权限请求
+//            rxPermissions.request(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE).subscribe(granted -> {
+//                if (granted) {//申请成功
+//                    showMsg("已获取权限");
+//                    hasPermissions = true;
+//                } else {//申请失败
+//                    showMsg("权限未开启");
+//                    hasPermissions = false;
+//                }
+//            });
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 555);
+
+    }
+
+
+    /**
+     * 更换头像
+     *
+     * @param view
+     */
+    public void changeAvatar(View view) {
+        bottomSheetDialog = new BottomSheetDialog(this);
+        bottomView = getLayoutInflater().inflate(R.layout.dialog_bottom, null);
+        bottomSheetDialog.setContentView(bottomView);
+        bottomSheetDialog.getWindow().findViewById(com.google.android.material.R.id.design_bottom_sheet).setBackgroundColor(Color.TRANSPARENT);
+        TextView tvTakePictures = bottomView.findViewById(R.id.tv_take_pictures);
+        TextView tvOpenAlbum = bottomView.findViewById(R.id.tv_open_album);
+        TextView tvCancel = bottomView.findViewById(R.id.tv_cancel);
+
+        //拍照
+        tvTakePictures.setOnClickListener(v -> {
+            takePhoto();
+            showMsg("拍照");
+            bottomSheetDialog.cancel();
+        });
+        //打开相册
+        tvOpenAlbum.setOnClickListener(v -> {
+            openAlbum();
+            showMsg("打开相册");
+            bottomSheetDialog.cancel();
+        });
+        //取消
+        tvCancel.setOnClickListener(v -> {
+            bottomSheetDialog.cancel();
+        });
+        bottomSheetDialog.show();
+    }
+
+    /**
+     * 拍照
+     */
+    private void takePhoto() {
+        if (!hasPermissions) {
+//            checkVersion();
+            showMsg("未获取到权限");
+            return;
+        }
+        SimpleDateFormat timeStampFormat = new SimpleDateFormat(
+                "yyyy_MM_dd_HH_mm_ss");
+        String filename = timeStampFormat.format(new Date());
+        outputImagePath = new File(getExternalCacheDir(),
+                filename + ".jpg");
+        Intent takePhotoIntent = CameraUtils.getTakePhotoIntent(this, outputImagePath);
+        // 开启一个带有返回值的Activity，请求码为TAKE_PHOTO
+        startActivityForResult(takePhotoIntent, TAKE_PHOTO);
+    }
+
+    /**
+     * 打开相册
+     */
+    private void openAlbum() {
+        if (!hasPermissions) {
+            showMsg("未获取到权限");
+//            checkVersion();
+            return;
+        }
+        startActivityForResult(CameraUtils.getSelectPhotoIntent(), SELECT_PHOTO);
+    }
+
+    /**
+     * 返回到Activity
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            //拍照后返回
+            case TAKE_PHOTO:
+                if (resultCode == RESULT_OK) {
+                    //显示图片
+                    displayImage(outputImagePath.getAbsolutePath());
+                }
+                break;
+            //打开相册后返回
+            case SELECT_PHOTO:
+                if (resultCode == RESULT_OK) {
+                    String imagePath = null;
+                    //判断手机系统版本号
+                    if (Build.VERSION.SDK_INT > Build.VERSION_CODES.KITKAT) {
+                        //4.4及以上系统使用这个方法处理图片
+                        imagePath = CameraUtils.getImageOnKitKatPath(data, this);
+                    } else {
+                        imagePath = CameraUtils.getImageBeforeKitKatPath(data, this);
+                    }
+                    //显示图片
+                    displayImage(imagePath);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    /**
+     * 通过图片路径显示图片
+     */
+    private void displayImage(String imagePath) {
+        if (!TextUtils.isEmpty(imagePath)) {
+            //显示图片
+            Glide.with(this).load(imagePath).apply(requestOptions).into(ib_portrait);
+
+            //压缩图片
+            orc_bitmap = CameraUtils.compression(BitmapFactory.decodeFile(imagePath));
+            //转Base64
+            base64Pic = BitmapUtils.bitmapToBase64(orc_bitmap);
+
+        } else {
+            showMsg("图片获取失败");
+        }
+    }
+
 }
